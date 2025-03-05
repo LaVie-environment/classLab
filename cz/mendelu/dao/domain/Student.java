ea83e623afffdb8d8d5a91673897ea67f2cc4ffc
Taofeek <bellotaophyc@outlook.com>
2025-03-05 08:34:07 +0100

    add the class

---
+++ cz/mendelu/dao/domain/Student.java
@@ -0,0 +1,35 @@
+package cz.mendelu.dao.domain;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id,String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Student(String name) {
+         this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
